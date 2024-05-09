98945220cfa087b0e43d2ea61ee0d1b9680c18c6
amrbadran <badran.amr2003@gmail.com>
2024-05-09 16:42:35 +0300

    End of HTML landing page

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*window.onscroll = function(){
+    fixedHeader();
+}
+
+
+function fixedHeader(){
+    let landing_header = document.querySelector('.landing-header');
+    let offset = landing_header.offsetTop;
+    if(window.pageYOffset > offset+800){
+        landing_header.classList.add('bg-dark');
+    }
+    else{
+        landing_header.classList.remove('bg-dark');
+    }
+}*/
