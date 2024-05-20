bb4f29bd1f4fef3d0c3052df5f9433c28e65ca39
amrbadran <badran.amr2003@gmail.com>
2024-05-20 19:39:28 +0300

    Shop page (Html And CSS )

--- js/script.js
+++ js/script.js
@@ -2,15 +2,24 @@ window.onscroll = function(){
     fixedHeader();
 }
 
+let rangeSlider = document.querySelector('.range-slider');
+rangeSlider.onchange = () => {
+    document.querySelector('.filter-price span').innerText = rangeSlider.value + '$';
+}
 
 function fixedHeader(){
     let landing_header = document.querySelector('.landing-header');
     let offset = landing_header.offsetTop;
-    let offset2 = document.querySelector('.section-wlc h1').offsetTop;
-    if(window.pageYOffset > offset2){
-        landing_header.classList.add('navbar-sticky-bg');
+    try{
+        let offset2 = document.querySelector('.section-wlc h1').offsetTop;
+        if(window.pageYOffset > offset2){
+            landing_header.classList.add('navbar-sticky-bg');
+        }
+        else{
+            landing_header.classList.remove('navbar-sticky-bg');
+        }
     }
-    else{
-        landing_header.classList.remove('navbar-sticky-bg');
+    catch (e) {
+        //
     }
 }
