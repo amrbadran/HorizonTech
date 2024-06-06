9a40e6627e4ae8ac141c1beeaf3cf4f517a36c4d
amrbadran <badran.amr2003@gmail.com>
2024-06-06 21:08:53 +0300

    some addition

--- js/script.js
+++ js/script.js
@@ -1,11 +1,42 @@
-window.onscroll = function(){
-    fixedHeader();
+let stats_vals = [];
+window.onload = () => {
+
+    let stat = document.querySelectorAll('.section-stats p');
+    stat.forEach((e)=>{stats_vals.push(e.innerText);e.innerText=0;})
 }
 
-let rangeSlider = document.querySelector('.range-slider');
-rangeSlider.onchange = () => {
-    document.querySelector('.filter-price span').innerText = rangeSlider.value + '$';
+let flag = false;
+window.onscroll = function(){
+    fixedHeader();
+    let x = document.querySelectorAll('.section-services .col-lg-4');
+    let services = document.querySelector('.section-services').offsetTop;
+    if(window.pageYOffset > services - 200){
+        x.forEach(element => {
+            element.style.cssText = "transform:translateX(0px);opacity:1;";
+        })
+    }else{
+        x.forEach(element => {
+            element.style.cssText = "transform:translateX(-200px);opacity:0;";
+        })
+    }
+
+    let stats = document.querySelector('.section-stats').offsetTop;
+
+    if(window.pageYOffset > stats - 1000 && !flag){
+        flag = true;
+        let stat = document.querySelectorAll('.section-stats p');
+        let i = 0;
+        stat.forEach((elem,idx) => {
+
+            let interval = setInterval(myFunc,5);
+            function myFunc(){
+                elem.innerText = +elem.innerText + 1;
+                if(elem.innerText === stats_vals[i]) {i++;clearInterval(interval);}
+            }
+        })
+    }
 }
+
 
 function fixedHeader(){
     let landing_header = document.querySelector('.landing-header');
